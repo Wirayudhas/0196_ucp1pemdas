9a06413fdb1bc9cf79bd2d6921bf3fd332cd8177
Wirayudhas <arturwirayuda@gmail.com>
2024-05-05 18:10:43 +0700

    Add project files.

---
+++ UCP1_pemdas/UCP1_pemdas.cpp
@@ -0,0 +1,49 @@
+//1.deklarasi variable bisa menggunakan float, int, string
+//2. conditional statmen pada c++ ada if else, penggunaanya dengan cara
+//if { keadaan yang di uji } else { syarat keadaan yg akan di uji}
+//3. struct bisa digunakan untuk menampung varible dan jenis data berupa int, string dan float
+//4. contoh prosedur menggunakan void() { fungsi yang akan dijalankan} 
+// contoh fungsi adalah for untuk menyimpan data yang telah dibuat
+//5. contoh implementasi dari looping {i=0, i<1, i++}
+
+
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+struct mahasiswa()
+{
+	string nama
+	int nilai
+	string status
+	int rerata
+	int mtk
+	int b.inggris
+}
+
+for (int i = 0. i < 20, i++) {
+	int main() {
+		Mahasiswa mhs;
+		cout << "Nama Mahasiswa: ";
+		cin >> mhs.nama;
+		cout << "nilai mtk: ";
+		cin >> mhs.mtk;
+		cout << "nilai b.inggris: ";
+		cin >> mhs.b.inggris;
+
+		rerata(mtk + b.inggris) / 2
+
+			cout << endl;
+		cout << "\n nilai nilai matematika: " << mhs.mtk;
+		cout << "\n nilai b.inggris: " << mhs.b.inggris;
+		cout << "\n status: " << mhs.status;
+		cout << "\n rerata: " << mhs.rerata;
+
+	}
+}
+
